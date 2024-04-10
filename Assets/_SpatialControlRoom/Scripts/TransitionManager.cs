050fe091344b0470d319db09bd427d64c64fed12
milanwulf <56889501+milanwulf@users.noreply.github.com>
2024-04-10 17:27:01 +0200

    added a UiFeedbackVisual

--- Assets/_SpatialControlRoom/Scripts/TransitionManager.cs
+++ Assets/_SpatialControlRoom/Scripts/TransitionManager.cs
@@ -10,17 +10,20 @@ public class TransitionManager : MonoBehaviour
     [Header("Hand Poses")]
     [SerializeField] private SelectorUnityEventWrapper leftScissorPose;
     [SerializeField] private SelectorUnityEventWrapper rightScissorPose;
+    [SerializeField] private UiFeedbackVisual feedbackVisual;
+    [SerializeField] private Transform leftHandAnchorTransform; //also gets L Controller position
+    [SerializeField] private Transform rightHandAnchorTransform; //also gets R Controller position
 
     private void OnEnable()
     {
-        leftScissorPose.WhenUnselected.AddListener(TriggerObsTransition);
-        rightScissorPose.WhenUnselected.AddListener(TriggerObsTransition);
+        leftScissorPose.WhenUnselected.AddListener(() => TriggerObsTransition(leftHandAnchorTransform));
+        rightScissorPose.WhenSelected.AddListener(() => TriggerObsTransition(rightHandAnchorTransform));
     }
 
     private void OnDisable()
     {
-        leftScissorPose.WhenUnselected.RemoveListener(TriggerObsTransition);
-        rightScissorPose.WhenUnselected.RemoveListener(TriggerObsTransition);
+        leftScissorPose.WhenUnselected.RemoveListener(() => TriggerObsTransition(leftHandAnchorTransform));
+        rightScissorPose.WhenSelected.RemoveListener(() => TriggerObsTransition(rightHandAnchorTransform));
     }
 
     private void Update()
@@ -29,19 +32,23 @@ public class TransitionManager : MonoBehaviour
         OVRInput.Controller activeController = OVRInput.GetActiveController();
 
         //only react to controller button presses and not hands
-        if (activeController == OVRInput.Controller.Touch)
+        if (activeController == OVRInput.Controller.RTouch || activeController == OVRInput.Controller.LTouch || activeController == OVRInput.Controller.Touch) //checks if only R or L controller is active or both
         {
-            if(OVRInput.GetDown(OVRInput.RawButton.X) || OVRInput.GetDown(OVRInput.RawButton.A))
+            if (OVRInput.GetDown(OVRInput.RawButton.X))
             {
-                Debug.Log("OVRInput GetDown is triggerd");
-                TriggerObsTransition();
+                TriggerObsTransition(leftHandAnchorTransform);
+            }
+            else if (OVRInput.GetDown(OVRInput.RawButton.A))
+            {
+                TriggerObsTransition(rightHandAnchorTransform);
             }
         }
     }
 
-    private void TriggerObsTransition()
+    private void TriggerObsTransition(Transform spawnLocation)
     {
         Debug.Log("Transition should be triggered");
         obsWebSocketManager.TriggerStudioModeTransition();
+        feedbackVisual.TriggerVisualFeedback("cut", spawnLocation);
     }
 }
